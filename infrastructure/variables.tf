8ce9c7e3379da7e1fd85e6f97f4dd831af50258e
nitishsingla1234 <49564392+nitishsingla1234@users.noreply.github.com>
2020-01-14 10:43:24 +0000

    Initial commit

---
+++ infrastructure/variables.tf
@@ -0,0 +1,15 @@
+variable "product" {}
+
+variable "component" {}
+
+variable "location" {
+  default = "UK South"
+}
+
+variable "env" {}
+
+variable "subscription" {}
+
+variable "common_tags" {
+  type = "map"
+}
