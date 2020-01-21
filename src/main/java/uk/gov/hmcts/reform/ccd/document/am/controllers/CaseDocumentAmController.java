1ac6716bb6768077a38daef7f0dbbc75972898c9
Nitish Singla <nitish.singla@hmcts.net>
2020-01-21 17:17:44 +0000

    Added a unit test file.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmController.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmController.java
@@ -10,7 +10,7 @@ import static org.springframework.http.ResponseEntity.ok;
  * Default endpoints per application.
  */
 @RestController
-public class CaseDocumentAMController {
+public class CaseDocumentAmController {
 
     /**
      * Root GET endpoint.
--- src/test/java/uk/gov/hmcts/reform/ccd/document/am/GetWelcomeTest.java
+++ src/test/java/uk/gov/hmcts/reform/ccd/document/am/GetWelcomeTest.java
@@ -3,20 +3,21 @@ package uk.gov.hmcts.reform.ccd.document.am;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import uk.gov.hmcts.reform.ccd.document.am.controllers.CaseDocumentAMController;
+import uk.gov.hmcts.reform.ccd.document.am.controllers.CaseDocumentAmController;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class GetWelcomeTest {
-    private final CaseDocumentAMController caseDocumentAMController = new CaseDocumentAMController();
+    private final CaseDocumentAmController caseDocumentAmController = new CaseDocumentAmController();
 
     @Test
     public void shouldReturnWelcomeMessage() {
-        ResponseEntity<String> caseDocumentControllerResponse = caseDocumentAMController.welcome();
-        assertNotNull(caseDocumentControllerResponse);
-        assertEquals(HttpStatus.OK, caseDocumentControllerResponse.getStatusCode());
-
+        ResponseEntity<String> caseDocumentControllerResponse = caseDocumentAmController.welcome();
+        assertNotNull(caseDocumentControllerResponse, "No Response from CaseDocumentAmController");
+        assertEquals(HttpStatus.OK, caseDocumentControllerResponse.getStatusCode(), "Status code is NOT OK");
+        assertEquals(caseDocumentControllerResponse.getBody(),
+            "Welcome to CCD Case Document AM Controller", "Response body does not have expected value");
     }
 }
