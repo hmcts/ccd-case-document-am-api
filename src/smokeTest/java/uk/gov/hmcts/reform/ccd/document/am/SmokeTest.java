6f9997f203fa2c8d109bc78e0ea4e50ac18d4c5e
SivaKanukolluQA <siva.kanukollu@hmcts.net>
2020-01-24 17:07:56 +0000

    AM-468: added sample smoke test.

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/ccd/document/am/SmokeTest.java
@@ -0,0 +1,36 @@
+package uk.gov.hmcts.reform.ccd.document.am;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+public class SmokeTest {
+
+    private final String targetInstance = StringUtils.defaultIfBlank(System.getenv("TEST_URL"),
+            "Need to update URL here");
+
+    @Test
+    public void should_receive_response_for_a_get_call() {
+
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+
+        Response response = SerenityRest
+            .given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .when()
+            .get("/")
+            .andReturn();
+        response.then().assertThat().statusCode(SC_OK);
+    }
+}
