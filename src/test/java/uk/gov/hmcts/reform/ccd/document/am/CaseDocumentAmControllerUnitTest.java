b48cb37eb7a17d7d20410bbf015b610621ff5fd4
Nitin <nitin.sahu@hmcts.net>
2020-01-24 16:26:54 +0000

    AM-467-JUnit-Integration-Changed from JUnit to Junit Jupiter

--- src/test/java/uk/gov/hmcts/reform/ccd/document/am/CaseDocumentAmControllerUnitTest.java
+++ src/test/java/uk/gov/hmcts/reform/ccd/document/am/CaseDocumentAmControllerUnitTest.java
@@ -1,16 +1,14 @@
 package uk.gov.hmcts.reform.ccd.document.am;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import uk.gov.hmcts.reform.ccd.document.am.controllers.CaseDocumentAmController;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 public class CaseDocumentAmControllerUnitTest {
@@ -19,18 +17,18 @@ public class CaseDocumentAmControllerUnitTest {
     @Test
     public void shouldReturnWelcomeMessage() {
         ResponseEntity<String> caseDocumentControllerResponse = caseDocumentAmController.welcome();
-        assertNotNull(caseDocumentControllerResponse);
-        assertEquals(HttpStatus.OK, caseDocumentControllerResponse.getStatusCode());
+        assertNotNull(caseDocumentControllerResponse, "No Response from CaseDocumentAmController");
+        assertEquals(HttpStatus.OK, caseDocumentControllerResponse.getStatusCode(), "Status code is NOT OK");
         assertEquals(caseDocumentControllerResponse.getBody(),
-            "Welcome to CCD Case Document AM Controller");
+            "Welcome to CCD Case Document AM Controller", "Response body does not have expected value");
     }
 
     @Test
     public void shoudReturnCaseNumbers() {
         ResponseEntity<List<String>> responseEntity = caseDocumentAmController.getCases();
-        assertNotNull(responseEntity);
-        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertEquals(5, responseEntity.getBody().size());
-        assertEquals("C101", responseEntity.getBody().get(0));
+        assertNotNull(responseEntity,"Response Enttity should not be null");
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode(), "Response Enttity should not be null");
+        assertEquals(5, responseEntity.getBody().size(), "Size should be 5");
+        assertEquals("C101", responseEntity.getBody().get(0), "Case Id check");
     }
 }
