d21626149d10e5d61082b28d110aa46f27fc9559
Nitin <nitin.sahu@hmcts.net>
2020-01-27 15:08:00 +0000

    AM-467-JUnit-Integration-Added serenity report for Integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmControllerIntegrationTest.java
@@ -1,13 +1,17 @@
 package uk.gov.hmcts.reform.ccd.document.am.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.web.context.WebApplicationContext;
@@ -15,13 +19,17 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.apache.http.HttpStatus.SC_OK;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 public class CaseDocumentAmControllerIntegrationTest {
 
     @Autowired
@@ -37,11 +45,24 @@ public class CaseDocumentAmControllerIntegrationTest {
     @Before
     public void setUp() {
         this.mockMvc = standaloneSetup(this.caseDocumentAmController).build();
+        final String targetInstance = StringUtils.defaultIfBlank(System.getenv("TEST_URL"),
+                                                                 "Need to update URL here");
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
     }
 
     @Test
     public void getWelComeAPI() throws Exception
     {
+        Response response = SerenityRest
+            .given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .when()
+            .get("/")
+            .andReturn();
+        response.then().assertThat().statusCode(SC_OK);
+
         final MvcResult result = mockMvc.perform(get("/").contentType(MediaType.APPLICATION_JSON)).
             andExpect(status().isOk()).andReturn();
         assertEquals("Welcome to CCD Case Document AM Controller", result.getResponse().getContentAsString());
@@ -50,6 +71,15 @@ public class CaseDocumentAmControllerIntegrationTest {
     @Test
     public void getCaseDetailsAPI() throws Exception
     {
+        Response response = SerenityRest
+            .given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .when()
+            .get("/")
+            .andReturn();
+        response.then().assertThat().statusCode(SC_OK);
+
         final MvcResult result  = mockMvc.perform(get("/cases/").contentType(MediaType.APPLICATION_JSON)).
             andExpect(status().isOk()).andReturn();
         List list = mapper.readValue(result.getResponse().getContentAsString(), ArrayList.class);
@@ -60,3 +90,4 @@ public class CaseDocumentAmControllerIntegrationTest {
         assertEquals("C105", list.get(4));
     }
 }
+
