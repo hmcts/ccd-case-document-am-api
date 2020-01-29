aab60abcaad222cd14c0fe81ddfd82fe7f37a97e
Nitish Singla <nitish.singla@hmcts.net>
2020-01-29 13:24:33 +0000

    Updated GetWelcomeTest

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/GetWelcomeTest.java
@@ -1,21 +1,34 @@
 package uk.gov.hmcts.reform.ccd.document.am.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GetWelcomeTest {
 
+    private MockMvc mockMvc;
+
     @Autowired
-    private transient MockMvc mockMvc;
+    private WebApplicationContext wac;
+
+    @BeforeEach
+    public  void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
 
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
