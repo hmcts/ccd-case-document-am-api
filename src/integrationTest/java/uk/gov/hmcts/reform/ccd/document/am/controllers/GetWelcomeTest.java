11bba7aa60d90b1a5dd87fdc79112835a48fde0e
Nitish Singla <nitish.singla@hmcts.net>
2020-01-29 13:30:39 +0000

    Updated GetWelcomeTest

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/GetWelcomeTest.java
@@ -3,10 +3,11 @@ package uk.gov.hmcts.reform.ccd.document.am.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -16,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class GetWelcomeTest {
 
