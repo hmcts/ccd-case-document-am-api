2520ad8cf78bfe627820b1ebd9428170c0f58750
Benjamin Matthews <benjamin.matthews@hmcts.net>
2020-01-30 15:36:05 +0000

    tidy up test

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/config/LaunchDarklyConfigurationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/config/LaunchDarklyConfigurationTest.java
@@ -1,14 +1,11 @@
 package uk.gov.hmcts.reform.ccd.document.am.config;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class LaunchDarklyConfigurationTest {
 
