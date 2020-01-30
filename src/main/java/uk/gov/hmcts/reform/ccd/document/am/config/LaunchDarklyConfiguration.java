e4a68c1c702c4f12788822629a6d28f2720550cc
Benjamin Matthews <benjamin.matthews@hmcts.net>
2020-01-30 15:28:03 +0000

    launchdarkly configuration

---
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/config/LaunchDarklyConfiguration.java
@@ -0,0 +1,35 @@
+package uk.gov.hmcts.reform.ccd.document.am.config;
+
+import com.launchdarkly.client.LDClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.io.IOException;
+
+@Configuration
+public class LaunchDarklyConfiguration {
+
+    private LDClient client;
+
+    @Value("${launchdarkly.sdk.key}")
+    private String sdkKey;
+
+    @PostConstruct
+    void initialise() {
+        client = new LDClient(sdkKey);
+    }
+
+    @Bean
+    public LDClient ldClient() {
+        return client;
+    }
+
+    @PreDestroy
+    void close() throws IOException {
+        client.flush();
+        client.close();
+    }
+}
