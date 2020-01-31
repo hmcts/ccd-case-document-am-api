c4f9309af50988f6d081215559d15de90bc8f548
Nitish Singla <nitish.singla@hmcts.net>
2020-01-31 16:46:56 +0000

    Disabling the functional test for now.

--- src/functionalTest/java/uk/gov/hmcts/reform/ccd/document/am/befta/CaseDocumentBeftaScenarioPlayer.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/ccd/document/am/befta/CaseDocumentBeftaScenarioPlayer.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.ccd.document.am.befta;
 
-import io.restassured.RestAssured;
 import uk.gov.hmcts.befta.player.DefaultBackEndFunctionalTestScenarioPlayer;
 
 public class CaseDocumentBeftaScenarioPlayer extends DefaultBackEndFunctionalTestScenarioPlayer {
