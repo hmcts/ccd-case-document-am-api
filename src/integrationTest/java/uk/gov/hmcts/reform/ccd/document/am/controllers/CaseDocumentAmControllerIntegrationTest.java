92b96254bbf5069c68e4a8fa244600749f470d31
Nitin <nitin.sahu@hmcts.net>
2020-02-02 20:31:13 +0000

    AM-468 Enabled integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmControllerIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmControllerIntegrationTest.java
@@ -1,29 +1,24 @@
-/*
+
 package uk.gov.hmcts.reform.ccd.document.am.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import net.serenitybdd.rest.SerenityRest;
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import java.util.List;
+import java.nio.charset.Charset;
 
-import static org.apache.http.HttpStatus.SC_OK;
 import static org.junit.Assert.assertEquals;
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
@@ -37,6 +32,14 @@ public class CaseDocumentAmControllerIntegrationTest {
 
     private MockMvc mockMvc;
 
+    @Value("${getwelcome.api.url}")
+    private String url;
+
+    private static final MediaType JSON_CONTENT_TYPE = new MediaType(
+        MediaType.APPLICATION_JSON.getType(),
+        MediaType.APPLICATION_JSON.getSubtype(),
+        Charset.forName("utf8"));
+
     @Autowired
     CaseDocumentAmController caseDocumentAmController;
 
@@ -44,53 +47,23 @@ public class CaseDocumentAmControllerIntegrationTest {
 
     @Before
     public void setUp() {
-        this.mockMvc = standaloneSetup(this.caseDocumentAmController).build();
-        final String targetInstance = StringUtils.defaultIfBlank(System.getenv("TEST_URL"),
-            "http://localhost:4455");
-        logger.info("\n\nCaseDocumentAmControllerIntegrationTest setUp targetInstance {}...", targetInstance);
-        RestAssured.baseURI = targetInstance;
+        RestAssured.baseURI = url;
         RestAssured.useRelaxedHTTPSValidation();
+        this.mockMvc = standaloneSetup(this.caseDocumentAmController).build();
     }
 
     @Test
     public void welComeAPITest() throws Exception {
-        logger.info("\n\nCaseDocumentAmControllerIntegrationTest : Inside  welComeAPITesti method...");
-        Response response = SerenityRest
+        logger.info("\n\nCaseDocumentAmControllerIntegrationTest : Inside  welComeAPITesti method...{}", url);
+        /*Response response = SerenityRest
             .given()
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .when()
-            .get("/")
+            .get(url);*/
+        final MvcResult result = mockMvc.perform(get(url).contentType(JSON_CONTENT_TYPE))
+            .andExpect(status().is(200))
             .andReturn();
-        response.then().assertThat().statusCode(SC_OK);
-        logger.info("\n\nwelComeAPITest...statusCode {} ", response.statusCode());
-        final MvcResult result = mockMvc.perform(get("/").contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk()).andReturn();
-        logger.info("\n\nCaseDocumentAmControllerIntegrationTest setUp MvcResult {}...", result);
-        logger.info("\n\nCaseDocumentAmControllerIntegrationTest setUp getContentAsString {}...", result.getResponse().getContentAsString());
         assertEquals("Assert for data", "Welcome to CCD Case Document AM Controller", result.getResponse().getContentAsString());
     }
-
-    @Test
-    public void caseDetailsAPITest() throws Exception {
-        Response response = SerenityRest
-            .given()
-            .relaxedHTTPSValidation()
-            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-            .when()
-            .get("/")
-            .andReturn();
-        response.then().assertThat().statusCode(SC_OK);
-        String message = "Assert for case value";
-        final MvcResult result = mockMvc.perform(get("/cases/").contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk()).andReturn();
-        List list = MAPPER.readValue(result.getResponse().getContentAsString(), List.class);
-        assertEquals(message, "C101", list.get(0));
-        assertEquals(message, "C102", list.get(1));
-        assertEquals(message, "C103", list.get(2));
-        assertEquals(message, "C104", list.get(3));
-        assertEquals(message, "C105", list.get(4));
-    }
 }
-
-*/
