d113b1caaed524cc3b6fb417aa0028569578f2eb
Nitish Singla <nitish.singla@hmcts.net>
2020-02-03 10:18:59 +0000

    Removing the unused imports.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmController.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/controllers/CaseDocumentAmController.java
@@ -1,14 +1,8 @@
 package uk.gov.hmcts.reform.ccd.document.am.controllers;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
