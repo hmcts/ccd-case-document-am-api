4be8c487c319dc835c81a27a950dddf69a31c110
Nitish Singla <nitish.singla@hmcts.net>
2020-02-06 16:29:58 +0000

    AM-500: Fixing the S2S authentication.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -96,7 +96,7 @@ module "ccd-case-document-am-api" {
 
     IDAM_USER_URL                       = "${var.idam_api_url}"
     IDAM_S2S_URL                        = "${local.s2s_url}"
-    DATA_STORE_IDAM_KEY                 = "${data.azurerm_key_vault_secret.ccd-case-document-am-api_s2s_key.value}"
+    CCD_DOCUMENT_API_IDAM_KEY           = "${data.azurerm_key_vault_secret.ccd-case-document-am-api_s2s_key.value}"
 
     CCD_DRAFT_ENCRYPTION_KEY            = "${random_string.draft_encryption_key.result}"
     DEFINITION_STORE_HOST               = "${local.definition_store_host}"
