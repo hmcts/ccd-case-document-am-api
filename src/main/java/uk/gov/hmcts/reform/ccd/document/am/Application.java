830109bae0f2f3c35a4cc4d790eff99f8a94d2c4
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-02-07 16:00:21 +0000

    AM-447- Modified the files to run Micro-service on container.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/Application.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/Application.java
@@ -3,8 +3,10 @@ package uk.gov.hmcts.reform.ccd.document.am;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement(proxyTargetClass = true)
 @EnableCircuitBreaker
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
