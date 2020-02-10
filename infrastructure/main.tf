a1709418232a81a1abe44487036fe977b85b3139
Nitish Singla <nitish.singla@hmcts.net>
2020-02-10 12:44:53 +0000

    AM-500: Adding the missing env variables in docker file.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -98,7 +98,7 @@ module "ccd-case-document-am-api" {
     IDAM_USER_URL                       = "${var.idam_api_url}"
     IDAM_S2S_URL                        = "${local.s2s_url}"
     CCD_DOCUMENT_API_IDAM_KEY           = "${data.azurerm_key_vault_secret.ccd_case_document_am_api_s2s_key.value}"
-
+    S2S_KEY                             = "${data.azurerm_key_vault_secret.ccd_case_document_am_api_s2s_key.value}"
     CCD_DRAFT_ENCRYPTION_KEY            = "${random_string.draft_encryption_key.result}"
     DEFINITION_STORE_HOST               = "${local.definition_store_host}"
 
