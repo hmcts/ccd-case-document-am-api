3cc4fe50b92a400e62e668fc8663f6061e3757ca
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-02-10 12:23:51 +0000

    This V1MediaType file is inherited from DM store and it is required in order to support HAL compliant responses received from DM Store.

---
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/configuration/V1MediaType.java
@@ -0,0 +1,24 @@
+package uk.gov.hmcts.reform.ccd.document.am.configuration;
+
+import org.springframework.http.MediaType;
+
+public class V1MediaType extends MediaType {
+
+
+    public static final String V1_HAL_DOCUMENT_COLLECTION_MEDIA_TYPE_VALUE = "application/vnd.uk.gov.hmcts.ccd-case-document-am-api.document-collection.v1+hal+json;charset=UTF-8";
+
+    public static final MediaType V1_HAL_DOCUMENT_COLLECTION_MEDIA_TYPE = valueOf(V1_HAL_DOCUMENT_COLLECTION_MEDIA_TYPE_VALUE);
+
+    public static final String V1_HAL_DOCUMENT_MEDIA_TYPE_VALUE = "application/vnd.uk.gov.hmcts.ccd-case-document-am-api.document.v1+hal+json;charset=UTF-8";
+
+    public static final MediaType V1_HAL_DOCUMENT_MEDIA_TYPE = valueOf(V1_HAL_DOCUMENT_MEDIA_TYPE_VALUE);
+
+
+
+    public V1MediaType(String type, String subtype) {
+        super(type, subtype);
+    }
+
+
+
+}
