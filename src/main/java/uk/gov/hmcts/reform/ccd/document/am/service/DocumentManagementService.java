7a4dd4e49de8fd65ee817b1c808b8a569c536de7
Nitin <nitin.sahu@hmcts.net>
2020-02-10 15:43:49 +0000

    AM-449 : fixed PMD and checkstlye for new package structure

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/DocumentManagementService.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/DocumentManagementService.java
@@ -7,7 +7,9 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface DocumentManagementService {
+
     /**
+     * Root GET endpoint.
      *
      * @param documentId Document Id
      * @return Optional containing document details including metadata when found; empty optional otherwise
@@ -15,16 +17,18 @@ public interface DocumentManagementService {
     Optional<StoredDocumentHalResource> getDocumentMetadata(final UUID documentId);
 
     /**
+     * Root GET endpoint.
      *
      * @param storedDocument This is the storedDocument response object returned by DM-store
      * @return String containing case id extracted from document metadata
-     */
+     **/
     String extractDocumentMetadata(StoredDocumentHalResource storedDocument);
 
     /**
+     * Root GET endpoint.
      *
      * @param documentId Document Id
      * @return OutputStream object containing binary content of document
-     */
+     **/
     ResponseEntity<?> getDocumentBinaryContent(final UUID documentId);
 }
