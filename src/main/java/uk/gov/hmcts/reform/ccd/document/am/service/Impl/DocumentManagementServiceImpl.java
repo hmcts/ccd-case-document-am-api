752cdd00881eaad29626a75703687faada6f7115
Aashish Aggarwal <aashish.aggarwal@hmcts.net>
2020-02-11 10:15:20 +0000

    Default service implementation for interacting with DM-store APIs.

---
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/Impl/DocumentManagementServiceImpl.java
@@ -0,0 +1,32 @@
+package uk.gov.hmcts.reform.ccd.document.am.service.Impl;
+
+import org.springframework.http.ResponseEntity;
+import uk.gov.hmcts.reform.ccd.document.am.model.StoredDocumentHalResource;
+import uk.gov.hmcts.reform.ccd.document.am.model.StoredDocumentHalResourceCollection;
+import uk.gov.hmcts.reform.ccd.document.am.model.UploadDocumentsCommand;
+import uk.gov.hmcts.reform.ccd.document.am.service.DocumentManagementService;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class DocumentManagementServiceImpl implements DocumentManagementService {
+    @Override
+    public Optional<StoredDocumentHalResource> getDocumentMetadata(UUID documentId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public String extractDocumentMetadata(StoredDocumentHalResource storedDocument) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<?> getDocumentBinaryContent(UUID documentId) {
+        return null;
+    }
+
+    @Override
+    public StoredDocumentHalResourceCollection uploadDocumentsContent(UploadDocumentsCommand uploadDocumentsContent) {
+        return null;
+    }
+}
