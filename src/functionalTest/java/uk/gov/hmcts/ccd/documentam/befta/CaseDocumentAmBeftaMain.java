a8e759ee183e4226973f43118d7547af75634fd3
mutlu <mutlu.sancaktutar@hmcts.net>
2020-02-24 16:06:15 +0000

    rename classes and packages with latest fw support

---
+++ src/functionalTest/java/uk/gov/hmcts/ccd/documentam/befta/CaseDocumentAmBeftaMain.java
@@ -0,0 +1,11 @@
+package uk.gov.hmcts.ccd.documentam.befta;
+
+import uk.gov.hmcts.befta.BeftaMain;
+
+public class CaseDocumentAmBeftaMain {
+
+    public static void main(String[] args) {
+        BeftaMain.main(args, new CaseDocumentAmTestAutomationAdapter());
+    }
+
+}
---
+++ src/functionalTest/java/uk/gov/hmcts/ccd/documentam/befta/TestDataLoaderMain.java
@@ -0,0 +1,9 @@
+package uk.gov.hmcts.ccd.documentam.befta;
+
+public class TestDataLoaderMain {
+
+    public static void main(String[] args) {
+        new CaseDocumentAmTestAutomationAdapter().loadTestDataIfNecessary();
+    }
+
+}
