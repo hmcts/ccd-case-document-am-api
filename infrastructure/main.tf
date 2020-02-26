c1810a18b24a8680c51ab94bd38576ba2de3e568
Nitish Singla <nitish.singla@hmcts.net>
2020-02-26 12:25:12 +0000

    Adding TEST_URL environment variable.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -110,7 +110,7 @@ module "ccd-case-document-am-api" {
     HTTP_CLIENT_MAX_CLIENT_PER_ROUTE      = "${var.http_client_max_client_per_route}"
     HTTP_CLIENT_VALIDATE_AFTER_INACTIVITY = "${var.http_client_validate_after_inactivity}"
     CASE_DOCUMENT_S2S_AUTHORISED_SERVICES = "${var.authorised-services}"
-    TEST_URL                              = "${var.test}"
+    TEST_URL                              = "${var.test_url}"
 
     JPA_CRITERIA_IN_SEARCH_ENABLED        = false
   }
