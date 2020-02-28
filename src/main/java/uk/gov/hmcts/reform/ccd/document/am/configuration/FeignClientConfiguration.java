939486120860feb9a11e95b6d433eb6f72c95f4c
Nitish Singla <nitish.singla@hmcts.net>
2020-02-28 14:15:01 +0000

    Fixing the service headers.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/configuration/FeignClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/configuration/FeignClientConfiguration.java
@@ -3,10 +3,13 @@ package uk.gov.hmcts.reform.ccd.document.am.configuration;
 import static uk.gov.hmcts.reform.ccd.document.am.apihelper.Constants.SERVICE_AUTHORIZATION;
 
 import java.util.Enumeration;
+import java.util.Locale;
 import javax.servlet.http.HttpServletRequest;
 
 import feign.RequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,6 +21,7 @@ import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
 @Slf4j
 public class FeignClientConfiguration {
 
+    private static final Logger LOG = LoggerFactory.getLogger(FeignClientConfiguration.class);
 
     private transient ServiceAuthTokenGenerator tokenGenerator;
 
@@ -35,16 +39,26 @@ public class FeignClientConfiguration {
                 HttpServletRequest request = attrs.getRequest();
                 Enumeration<String> headerNames = request.getHeaderNames();
                 if (headerNames != null) {
+                    System.out.println(" headerNames is : " + headerNames);
+                    LOG.error("headerNames is : " + headerNames);
                     while (headerNames.hasMoreElements()) {
-
                         String name = headerNames.nextElement();
                         String value = request.getHeader(name);
                         System.out.println("Header Name :" + name + "     Value : " + value);
-                        requestTemplate.header(name, value);
+                        if (config.getHeaders().contains(name.toLowerCase(Locale.ENGLISH))) {
+                            System.out.println("Config headers has the name " + name );
+                            if (name.equals(SERVICE_AUTHORIZATION)) {
+                                requestTemplate.header(name, tokenGenerator.generate());
+                            } else {
+                                requestTemplate.header(name, value);
+                            }
+                        }
+
                     }
-                    String serviceToken = tokenGenerator.generate();
+                    /*String serviceToken = tokenGenerator.generate();
                     System.out.println("Generated ServiceToken is : " + serviceToken);
-                    requestTemplate.header(SERVICE_AUTHORIZATION, serviceToken);
+                    LOG.error("Generated ServiceToken is : " + serviceToken);
+                    requestTemplate.header(SERVICE_AUTHORIZATION, serviceToken);*/
 
                 } else {
                     log.warn("FeignHeadConfiguration", "Failed to get request header!");
