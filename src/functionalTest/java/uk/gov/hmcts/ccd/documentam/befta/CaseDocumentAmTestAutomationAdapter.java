aac53397bea279e8c8e3cea7a3cdca5fd7716cd4
Nitish Singla <nitish.singla@hmcts.net>
2020-02-28 12:02:40 +0000

    trying to fix the develoment,

--- src/functionalTest/java/uk/gov/hmcts/ccd/documentam/befta/CaseDocumentAmTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/ccd/documentam/befta/CaseDocumentAmTestAutomationAdapter.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.ccd.documentam.befta;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
 import uk.gov.hmcts.befta.util.ReflectionUtils;
 
 public class CaseDocumentAmTestAutomationAdapter extends DefaultTestAutomationAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(CaseDocumentAmTestAutomationAdapter.class);
 
     private transient TestDataLoaderToDefinitionStore loader = new TestDataLoaderToDefinitionStore(this);
 
@@ -21,10 +25,11 @@ public class CaseDocumentAmTestAutomationAdapter extends DefaultTestAutomationAd
             //actualResponse][body][_embedded][documents][0][_links][self][href]
             try {
                 String href = (String) ReflectionUtils
-                    .deepGetFieldInObject(scenarioContext, "testData.actualResponse.body._embedded.documents[0]._links.self.href");
+                    .deepGetFieldInObject(scenarioContext,
+                                          "testData.actualResponse.body._embedded.documents[0]._links.self.href");
                 return href.substring(href.length() - 36);
-            } catch (Exception e) {
-                e.printStackTrace();
+            } catch (Exception exception) {
+                logger.error("Exception while getting the Document ID from the response", exception.getMessage());
                 return "Error extracting the Document Id";
             }
         }
