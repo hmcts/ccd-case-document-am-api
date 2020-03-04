1dc64ad98224d2c81d5fd91b22525f978ba5570b
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-03-04 09:56:08 +0000

    AM-575- removed default one.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/configuration/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/configuration/SwaggerConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
@@ -12,7 +11,6 @@ import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import uk.gov.hmcts.reform.ccd.document.am.Application;
 import uk.gov.hmcts.reform.ccd.document.am.controller.endpoints.CaseDocumentAmController;
 
 import java.util.Arrays;
@@ -20,16 +18,6 @@ import java.util.Arrays;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-            .useDefaultResponseMessages(false)
-            .select()
-            .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
-            .paths(PathSelectors.any())
-            .build();
-    }
 
     @Bean
     public Docket apiV2() {
