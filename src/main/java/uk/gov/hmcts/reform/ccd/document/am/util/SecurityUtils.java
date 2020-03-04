839779d7cac45a28565090fa249e3f96f88e7bdb
Nitish Singla <nitish.singla@hmcts.net>
2020-03-04 12:43:17 +0000

    Fixing the token generation.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/util/SecurityUtils.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/util/SecurityUtils.java
@@ -6,7 +6,6 @@ import feign.Feign;
 import feign.jackson.JacksonEncoder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.GrantedAuthority;
