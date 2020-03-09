3198e9f68ea7a34734640c7e78dbea4fa84b7248
ConorH994 <conor.halpin@hmcts.net>
2020-03-09 16:13:40 +0000

    CaseDocumentControllerAdvice Test coverage added

---
+++ src/test/java/uk/gov/hmcts/reform/ccd/document/am/controller/advice/CaseDocumentControllerAdviceTest.java
@@ -0,0 +1,83 @@
+package uk.gov.hmcts.reform.ccd.document.am.controller.advice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConversionException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import uk.gov.hmcts.reform.ccd.document.am.controller.advice.exception.InvalidRequest;
+import uk.gov.hmcts.reform.ccd.document.am.controller.advice.exception.RequiredFieldMissingException;
+import uk.gov.hmcts.reform.ccd.document.am.controller.advice.exception.ResourceNotFoundException;
+import uk.gov.hmcts.reform.ccd.document.am.controller.advice.exception.UnauthorizedException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import javax.servlet.http.HttpServletRequest;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CaseDocumentControllerAdviceTest {
+
+    private CaseDocumentControllerAdvice csda = new CaseDocumentControllerAdvice();
+    private HttpServletRequest servletRequestMock = mock(HttpServletRequest.class);
+
+    @Test
+    public void handleUnautorizedExceptionException() {
+        UnauthorizedException unauthorizedException = mock(UnauthorizedException.class);
+        ResponseEntity<Object> responseEntity = csda.handleUnautorizedExceptionException(servletRequestMock, unauthorizedException);
+        assertEquals(HttpStatus.UNAUTHORIZED, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void handleRequiredFieldMissingException() {
+        RequiredFieldMissingException requiredFieldMissingException = mock(RequiredFieldMissingException.class);
+        ResponseEntity<Object> responseEntity = csda.handleRequiredFieldMissingException(servletRequestMock, requiredFieldMissingException);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void customValidationError() {
+        InvalidRequest invalidRequestException = mock(InvalidRequest.class);
+        ResponseEntity<Object> responseEntity = csda.customValidationError(invalidRequestException);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void handleMethodArgumentNotValidException() {
+        MethodArgumentNotValidException methodArgumentNotValidException = mock(MethodArgumentNotValidException.class);
+        ResponseEntity<Object> responseEntity = csda.handleMethodArgumentNotValidException(servletRequestMock, methodArgumentNotValidException);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void handleResourceNotFoundException() {
+        ResourceNotFoundException resourceNotFoundException = mock(ResourceNotFoundException.class);
+        ResponseEntity<Object> responseEntity = csda.handleResourceNotFoundException(servletRequestMock,resourceNotFoundException);
+        assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void handleHttpMessageConversionException() {
+        HttpMessageConversionException httpMessageConversionException = mock(HttpMessageConversionException.class);
+        ResponseEntity<Object> responseEntity = csda.handleHttpMessageConversionException(servletRequestMock, httpMessageConversionException);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void handleUnknownException() {
+        Exception exception = mock(Exception.class);
+        ResponseEntity<Object> responseEntity = csda.handleUnknownException(servletRequestMock, exception);
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void getTimeStamp() {
+        String time = csda.getTimeStamp();
+        assertEquals(time.substring(0,16), new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.ENGLISH).format(new Date()));
+    }
+}
