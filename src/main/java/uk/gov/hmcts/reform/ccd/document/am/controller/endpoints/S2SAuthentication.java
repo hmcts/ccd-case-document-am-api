12f588c2ff8d877ad268e574da69078870b95bc9
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-03-09 12:26:48 +0000

    AM-582- generate token.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/controller/endpoints/S2SAuthentication.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/controller/endpoints/S2SAuthentication.java
@@ -1,17 +1,25 @@
 package uk.gov.hmcts.reform.ccd.document.am.controller.endpoints;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 public class S2SAuthentication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(S2SAuthentication.class);
+    private transient AuthTokenGenerator authTokenGenerator;
+
     @RequestMapping(value = "/testS2SAuthorization", method = RequestMethod.GET)
     public ResponseEntity<String> testS2SAuthorization() {
+
+        LOG.info("Token Generation ", authTokenGenerator.generate()); 
 
         return ok("S2S Authentication is successful !!");
     }
