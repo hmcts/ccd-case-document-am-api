507f7e607110e7b9285ed1f9f786a8c26a12c0a0
Nitish Singla <nitish.singla@hmcts.net>
2020-03-12 17:52:04 +0000

    Reverted the microservice name.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/impl/CaseDataStoreServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/impl/CaseDataStoreServiceImpl.java
@@ -1,19 +1,28 @@
 package uk.gov.hmcts.reform.ccd.document.am.service.impl;
 
+import java.util.LinkedHashMap;
+import java.util.Optional;
+import java.util.UUID;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
 import uk.gov.hmcts.reform.ccd.document.am.controller.advice.exception.ForbiddenException;
 import uk.gov.hmcts.reform.ccd.document.am.model.CaseDocumentMetadata;
-import uk.gov.hmcts.reform.ccd.document.am.model.Document;
-import uk.gov.hmcts.reform.ccd.document.am.model.enums.Permission;
 import uk.gov.hmcts.reform.ccd.document.am.service.CaseDataStoreService;
-
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.UUID;
+import uk.gov.hmcts.reform.ccd.document.am.util.SecurityUtils;
 
 @Slf4j
 @Service
@@ -21,18 +30,53 @@ public class CaseDataStoreServiceImpl implements CaseDataStoreService {
 
     private static final Logger LOG = LoggerFactory.getLogger(CaseDataStoreServiceImpl.class);
 
-    @Override
-    public Optional<CaseDocumentMetadata> getCaseDocumentMetadata(String caseId, UUID documentId) {
-        Document document = Document.builder().permissions(Arrays.asList(Permission.CREATE,Permission.READ)).id(documentId.toString()).build();
+    @Value("${caseDataStoreUrl}")
+    private transient String caseDataStoreUrl;
+    private RestTemplate restTemplate;
+    private transient SecurityUtils securityUtils;
 
-        Optional<CaseDocumentMetadata> caseDocumentMetadata = Optional.of(CaseDocumentMetadata.builder().caseId(caseId).document(Optional.of(document))
-                                                              .build());
-        if (!caseDocumentMetadata.get().getDocument().isPresent()) {
-            LOG.error("Document couldn't be found on case reference : " + caseId + ", response code from CCD : " + HttpStatus.FORBIDDEN);
-            throw new ForbiddenException(documentId.toString());
-        } else {
-            return caseDocumentMetadata;
+
+    @Autowired
+    public CaseDataStoreServiceImpl(RestTemplate restTemplate, SecurityUtils securityUtils) {
+        this.restTemplate = restTemplate;
+        this.securityUtils = securityUtils;
+    }
+
+    @Override
+    public Optional<CaseDocumentMetadata> getCaseDocumentMetadata(String caseId, UUID documentId, String authorization) {
+        HttpHeaders headers = prepareRequestForUpload(authorization);
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> responseEntity =
+            restTemplate.exchange(caseDataStoreUrl
+                                      .concat("/cases/")
+                                      .concat(caseId)
+                                      .concat("/documents/")
+                                      .concat(documentId.toString()),
+                                  HttpMethod.GET, requestEntity, Object.class);
+
+        if (responseEntity.getStatusCode() == HttpStatus.OK
+            && responseEntity.getBody() instanceof LinkedHashMap) {
+            LinkedHashMap<String, Object> responseObject = (LinkedHashMap) responseEntity.getBody();
+            CaseDocumentMetadata caseDocumentMetadata = new ObjectMapper().convertValue(responseObject.get("documentMetadata"),
+                                                                                        CaseDocumentMetadata.class);
+            if (null == caseDocumentMetadata.getDocument()) {
+                LOG.error("Could't find document for case  : " + caseId + ", response code from CCD : " + HttpStatus.FORBIDDEN);
+                throw new ForbiddenException("Could't find document for case  : " + caseId);
+            }
+            return Optional.of(caseDocumentMetadata);
         }
 
+        return Optional.empty();
+    }
+
+    private HttpHeaders prepareRequestForUpload(String authorization) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.addAll(securityUtils.authorizationHeaders());
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("experimental", "true");
+        headers.set("Authorization", authorization);
+        return headers;
     }
 }
