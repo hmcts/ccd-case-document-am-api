1af3e9bcf508cd51f6cbafec86b88a7364e6ca4f
ConorH994 <conor.halpin@hmcts.net>
2020-03-15 16:28:44 +0000

    added CaseData Store Service Test

---
+++ src/test/java/uk/gov/hmcts/reform/ccd/document/am/service/impl/CaseDataStoreServiceImplTest.java
@@ -0,0 +1,31 @@
+package uk.gov.hmcts.reform.ccd.document.am.service.impl;
+
+import org.junit.jupiter.api.Test;
+import uk.gov.hmcts.reform.ccd.document.am.model.CaseDocumentMetadata;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+class CaseDataStoreServiceImplTest {
+
+    private static final String CASE_ID = "1582550122096256";
+    private static final String MATCHED_DOCUMENT_ID = "41334a2b-79ce-44eb-9168-2d49a744be9c";
+
+    private CaseDataStoreServiceImpl sut = new CaseDataStoreServiceImpl();
+
+    private Optional<CaseDocumentMetadata> caseDocumentMetadata;
+
+    @Test
+    void getCaseDocumentMetadataSuccess() {
+        caseDocumentMetadata = sut.getCaseDocumentMetadata(CASE_ID, getUuid(MATCHED_DOCUMENT_ID));
+        assertNotNull(caseDocumentMetadata);
+        assertEquals(CASE_ID,caseDocumentMetadata.get().getCaseId());
+        assertEquals(MATCHED_DOCUMENT_ID,caseDocumentMetadata.get().getDocument().get().getId());
+    }
+
+    private UUID getUuid(String id) {
+        return UUID.fromString(id);
+    }
+}
