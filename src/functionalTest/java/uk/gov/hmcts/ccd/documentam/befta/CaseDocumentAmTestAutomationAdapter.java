4a15e70f366154f8ee4610b9d2c2e1d9a60d3d80
mutlu <mutlu.sancaktutar@hmcts.net>
2020-03-23 16:56:37 +0000

    introduce basic logic for the custom values

--- src/functionalTest/java/uk/gov/hmcts/ccd/documentam/befta/CaseDocumentAmTestAutomationAdapter.java
+++ src/functionalTest/java/uk/gov/hmcts/ccd/documentam/befta/CaseDocumentAmTestAutomationAdapter.java
@@ -2,9 +2,12 @@ package uk.gov.hmcts.ccd.documentam.befta;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import uk.gov.hmcts.befta.BeftaMain;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 import uk.gov.hmcts.befta.dse.ccd.TestDataLoaderToDefinitionStore;
 import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
+import uk.gov.hmcts.befta.util.EnvironmentVariableUtils;
 import uk.gov.hmcts.befta.util.ReflectionUtils;
 
 public class CaseDocumentAmTestAutomationAdapter extends DefaultTestAutomationAdapter {
@@ -21,6 +24,7 @@ public class CaseDocumentAmTestAutomationAdapter extends DefaultTestAutomationAd
 
     @Override
     public Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
+        String docAmUrl = EnvironmentVariableUtils.getRequiredVariable("CASE_DOC_AM_URL");
         if (key.equals("documentIdInTheResponse")) {
             try {
                 String href = (String) ReflectionUtils
@@ -32,6 +36,26 @@ public class CaseDocumentAmTestAutomationAdapter extends DefaultTestAutomationAd
                 return "Error extracting the Document Id";
             }
         }
+        else if(key.equals(key.equals("S_040_validSelfLink"))) {
+            String self = (String) ReflectionUtils
+                    .deepGetFieldInObject(scenarioContext,
+                            "testData.actualResponse.body.links.self.href");
+            if(self!=null && self.startsWith(docAmUrl+"/cases/documents/"))
+                return self;
+            return "docAmUrl"+"/cases/documents/<a document id>";
+            
+        }
+        else if(key.equals(key.equals("S_040_validBinaryLink"))) {
+            String binary = (String) ReflectionUtils
+                    .deepGetFieldInObject(scenarioContext,
+                            "testData.actualResponse.body.links.binary.href");
+            if(binary!=null && binary.startsWith(docAmUrl+"/cases/documents/") && binary.endsWith("/binary"))
+                return binary;
+            return "docAmUrl"+"/cases/documents/<a document id>/binary";
+            
+        }
         return super.calculateCustomValue(scenarioContext, key);
     }
 }
+
+"href":"{{CASE_DOC_AM_URL}}/cases/documents/${[scenarioContext][childContexts][Default_Document_Upload_Data][customValues][documentIdInTheResponse]}"},"binary":{"href":"{{CASE_DOC_AM_URL}}/cases/documents/${[scenarioContext][childContexts][Default_Document_Upload_Data][customValues][documentIdInTheResponse]}/binary"
