226d56593588addef310aeee4d194bc69e06d582
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-03-25 17:48:50 +0000

    AM-620 synch with development branch.

--- src/smokeTest/java/uk/gov/hmcts/reform/ccd/document/am/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/ccd/document/am/SmokeTest.java
@@ -1,41 +1,26 @@
 package uk.gov.hmcts.reform.ccd.document.am;
 
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
-import com.microsoft.applicationinsights.boot.dependencies.apachecommons.lang3.StringUtils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
-import org.hamcrest.Matchers;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RunWith(SpringIntegrationSerenityRunner.class)
-//@ConfigurationProperties
-public class SmokeTest extends BaseTest {
+public class SmokeTest {
 
-    @Value("${idam.s2s-auth.totp_secret}")
-    String secret;
-    @Value("${idam.s2s-auth.microservice}")
-    String microService;
-    @Value("${idam.s2s-auth.url}")
-    String s2sUrl;
-
-    private static final String baseURI = StringUtils.defaultIfBlank(System.getenv("CASE_DOCUMENT_AM_URL"),
+    private static final String targetInstance = StringUtils.defaultIfBlank(System.getenv("TEST_URL"),
         "http://localhost:4455");
 
     @Test
-    public void should_receive_response_for_a_get_document_meta_data() {
-
-        String serviceAuth = new BaseTest().authTokenGenerator(secret, microService, generateServiceAuthorisationApi(s2sUrl)).generate();
-
-        String targetInstance = baseURI + "/cases/documents/00000000-0000-0000-0000-000000000000";
+    public void should_receive_response_for_a_get_call() {
 
         RestAssured.baseURI = targetInstance;
         RestAssured.useRelaxedHTTPSValidation();
@@ -44,34 +29,10 @@ public class SmokeTest extends BaseTest {
             .given()
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-            .header("user-roles", "caseworker")
-            .header("ServiceAuthorization", "Bearer " + serviceAuth)
             .when()
             .get("/")
             .andReturn();
-        response.then().assertThat().statusCode(HttpStatus.NOT_FOUND.value())
-            .body("message", Matchers.equalTo("Resource not found 00000000-0000-0000-0000-000000000000"));
-    }
-
-    @Test
-    public void should_receive_response_for_a_get_document_binary() {
-
-        String targetInstance = baseURI + "/cases/documents/00000000-0000-0000-0000-000000000000/binary";
-
-        RestAssured.baseURI = targetInstance;
-        RestAssured.useRelaxedHTTPSValidation();
-
-        Response response = SerenityRest
-            .given()
-            .relaxedHTTPSValidation()
-            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-            .header("user-roles", "caseworker")
-            .header("ServiceAuthorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJjY2RfZ3ciLCJleHAiOjE1ODQ2Mjg1ODl9."
-                + "FFxdkaELH1Hip7qaLQaDqQj_gFYTZuU5SnQTT7s2Od4Fz2d9K4Qj2TaxMEMKx0eK8PMSO0IscSpLKUAGjJ4-tw")
-            .when()
-            .get("/")
-            .andReturn();
-        response.then().assertThat().statusCode(HttpStatus.NOT_FOUND.value())
-            .body("message", Matchers.equalTo("Resource not found 00000000-0000-0000-0000-000000000000"));
+        response.then().assertThat().statusCode(SC_OK);
+        assertEquals("Assert for data", "Smoke Test", "Smoke Test");
     }
 }
