9808c636ed939de375c774570ad963a38333afe7
Nitish Singla <nitish.singla@hmcts.net>
2020-03-26 13:09:14 +0000

    Added the metadata JurisdictionId and CaseTypeId.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/common/ValidationService.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/common/ValidationService.java
@@ -90,7 +90,6 @@ public class ValidationService {
     public static void validateDocumentId(String documentId) {
         try {
             UUID uuid = UUID.fromString(documentId);
-
         } catch (IllegalArgumentException exception) {
             throw new IllegalArgumentException(String.format("The input parameter: %s is not a valid UUID", documentId));
         }
