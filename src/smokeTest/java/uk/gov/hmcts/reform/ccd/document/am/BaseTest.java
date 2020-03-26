14325d9cb78aab62c084dc6418bf8a712a5db6ec
SivaKanukolluQA <siva.kanukollu@hmcts.net>
2020-03-26 21:52:01 +0000

    AM-607: Smoke tests.

--- src/smokeTest/java/uk/gov/hmcts/reform/ccd/document/am/BaseTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/ccd/document/am/BaseTest.java
@@ -1,18 +1,12 @@
 package uk.gov.hmcts.reform.ccd.document.am;
 
 import feign.Feign;
-import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.befta.BeftaMain;
-import uk.gov.hmcts.befta.auth.AuthApi;
-import uk.gov.hmcts.befta.auth.OAuth2;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.ServiceAuthTokenGenerator;
-
-import java.util.Base64;
 
 @Service
 public class BaseTest {
@@ -21,34 +15,18 @@ public class BaseTest {
     private static final String CODE = "code";
     private static final String BASIC = "Basic ";
 
-
-    private static  AuthApi idamApi  = Feign.builder().encoder(new JacksonEncoder()).decoder(new JacksonDecoder()).target(AuthApi.class, BeftaMain.getConfig().getIdamURL());
-
-
     public ServiceAuthorisationApi generateServiceAuthorisationApi(@Value("${idam.s2s-auth.url}") final String s2sUrl) {
         return Feign.builder()
-                    .encoder(new JacksonEncoder())
-                    .contract(new SpringMvcContract())
-                    .target(ServiceAuthorisationApi.class, s2sUrl);
+            .encoder(new JacksonEncoder())
+            .contract(new SpringMvcContract())
+            .target(ServiceAuthorisationApi.class, s2sUrl);
     }
 
     public ServiceAuthTokenGenerator authTokenGenerator(
         @Value("${idam.s2s-auth.totp_secret}") final String secret,
         @Value("${idam.s2s-auth.microservice}") final String microService,
         final ServiceAuthorisationApi serviceAuthorisationApi
-                                                       ) {
+    ) {
         return new ServiceAuthTokenGenerator(secret, microService, serviceAuthorisationApi);
     }
-
-    public static String getIdamOauth2Token(String username, String password) {
-        String authorisation = username + ":" + password;
-        String base64Authorisation = Base64.getEncoder().encodeToString(authorisation.getBytes());
-        OAuth2 oauth2 = BeftaMain.getConfig().getOauth2Config();
-        AuthApi.AuthenticateUserResponse authenticateUserResponse = idamApi
-            .authenticateUser(BASIC + base64Authorisation, CODE, oauth2.getClientId(), oauth2.getRedirectUri());
-        AuthApi.TokenExchangeResponse tokenExchangeResponse = idamApi.exchangeCode(authenticateUserResponse.getCode(),
-            AUTHORIZATION_CODE, oauth2.getClientId(), oauth2.getClientSecret(), oauth2.getRedirectUri());
-        return tokenExchangeResponse.getAccessToken();
-    }
-
 }
--- src/smokeTest/java/uk/gov/hmcts/reform/ccd/document/am/SmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/ccd/document/am/SmokeTest.java
@@ -1,8 +1,5 @@
 package uk.gov.hmcts.reform.ccd.document.am;
 
-import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 import com.microsoft.applicationinsights.boot.dependencies.apachecommons.lang3.StringUtils;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
@@ -14,10 +11,12 @@ import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.hmcts.reform.ccd.document.am.utils.IdamUtils;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @SpringBootTest
 @RunWith(SpringIntegrationSerenityRunner.class)
@@ -31,19 +30,21 @@ public class SmokeTest extends BaseTest {
     @Value("${idam.s2s-auth.url}")
     String s2sUrl;
 
+    IdamUtils idamUtils =  new IdamUtils();
+
+    String username = "befta.caseworker.2.solicitor.2@gmail.com";
+    String password = "Pa55word11";
+    String userToken = idamUtils.getIdamOauth2Token(username, password);
+    String documentId = "00000000-0000-0000-0000-000000000000";
+
     private static final String baseURI = StringUtils.defaultIfBlank(System.getenv("CASE_DOCUMENT_AM_URL"),
         "http://localhost:4455");
-
 
 
     @Test
     public void should_receive_response_for_a_get_document_meta_data() {
 
-        String serviceAuth = new BaseTest().authTokenGenerator(secret, microService,
-            generateServiceAuthorisationApi(s2sUrl)).generate();
-        String targetInstance = baseURI + "/cases/documents/00000000-0000-0000-0000-000000000000";
-
-        RestAssured.baseURI = targetInstance;
+        RestAssured.baseURI = baseURI + "/cases/documents/" + documentId;
         RestAssured.useRelaxedHTTPSValidation();
 
         Response response = SerenityRest
@@ -51,98 +52,89 @@ public class SmokeTest extends BaseTest {
             .relaxedHTTPSValidation()
             .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
             .header("user-roles", "caseworker")
-            .header( "ServiceAuthorization", "Bearer " + serviceAuth)
-            .header( "Authorization", "Bearer " + "Authorization")
+            .header("ServiceAuthorization", "Bearer " + getServiceAuth())
+            .header("Authorization", "Bearer " + userToken)
             .when()
             .get("/")
             .andReturn();
-        response.then().assertThat().statusCode( HttpStatus.NOT_FOUND.value())
-            .body("message", Matchers.equalTo("Resource not found 00000000-0000-0000-0000-000000000000"));
+        response.then().assertThat().statusCode(HttpStatus.NOT_FOUND.value())
+            .body("message", Matchers.equalTo("Resource not found " + documentId));
     }
-//
-//    @Test
-//    public void should_receive_response_for_a_get_document_binary() {
-//
-//        String targetInstance = baseURI + "/cases/documents/00000000-0000-0000-0000-000000000000/binary";
-//        String serviceAuth = new BaseTest().authTokenGenerator(secret, microService,
-//            generateServiceAuthorisationApi(s2sUrl)).generate();
-//
-//        RestAssured.baseURI = targetInstance;
-//        RestAssured.useRelaxedHTTPSValidation();
-//
-//        Response response = SerenityRest
-//            .given()
-//            .relaxedHTTPSValidation()
-//            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-//            .header( "ServiceAuthorization", "Bearer " + serviceAuth)
-//            .header( "Authorization", "Bearer " + BaseTest.getIdamOauth2Token("befta.caseworker.2.solicitor.2@gmail.com", "Pa55word11"))
-//            .when()
-//            .get("/")
-//            .andReturn();
-//        response.then().assertThat().statusCode( HttpStatus.NOT_FOUND.value())
-//            .body("message", Matchers.equalTo("Resource not found 00000000-0000-0000-0000-000000000000"));
-//    }
-//
-//    @Test
-//    public void should_receive_response_for_patch_ttl() {
-//
-//        String targetInstance = baseURI + "/cases/documents/00000000-0000-0000-0000-000000000000";
-//        String serviceAuth = new BaseTest().authTokenGenerator(secret, microService,
-//            generateServiceAuthorisationApi(s2sUrl)).generate();
-//
-//        JSONObject requestBody = new JSONObject();
-//        requestBody.put("ttl", "2025-10-31T10:10:10+0000");
-//
-//        RestAssured.baseURI = targetInstance;
-//        RestAssured.useRelaxedHTTPSValidation();
-//
-//        Response response = SerenityRest
-//            .given()
-//            .relaxedHTTPSValidation()
-//            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-//            .header( "ServiceAuthorization", "Bearer " + serviceAuth)
-//            .header( "Authorization", "Bearer " + "Authorization")
-//            .body(requestBody.toString())
-//            .when()
-//            .patch("/")
-//            .andReturn();
-//        response.then().assertThat().statusCode( HttpStatus.NOT_FOUND.value())
-//            .body("message", Matchers.equalTo("Resource not found 00000000-0000-0000-0000-000000000000"));
-//    }
-//
-//    @Test
-//    public void should_receive_response_for_patch_attach_to_document() {
-//
-//        String documentId = "00000000-0000-0000-0000-000000000000";
-//        String targetInstance = baseURI + "/cases/documents/"+ documentId;
-//        String serviceAuth = new BaseTest().authTokenGenerator(secret, microService,
-//            generateServiceAuthorisationApi(s2sUrl)).generate();
-//
-//        JSONObject document1 = new JSONObject();
-//        document1.put("id", documentId);
-//        document1.put("hashToken", "3aba5fe28560118793e7f086b442ec40f933c8607ac4e32dc4adf865a0be41c2");
-//
-//        JSONArray documents = new JSONArray();
-//        documents.put(document1);
-//
-//        JSONObject requestBody = new JSONObject();
-//        requestBody.put("caseId", "1234123412341234");
-//        requestBody.put("documents", documents);
-//
-//        RestAssured.baseURI = targetInstance;
-//        RestAssured.useRelaxedHTTPSValidation();
-//
-//        Response response = SerenityRest
-//            .given()
-//            .relaxedHTTPSValidation()
-//            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
-//            .header( "ServiceAuthorization", "Bearer " + serviceAuth)
-//            .header( "Authorization", "Bearer " + "Authorization")
-//            .body(requestBody.toString())
-//            .when()
-//            .patch("/")
-//            .andReturn();
-//        response.then().assertThat().statusCode( HttpStatus.NOT_FOUND.value())
-//            .body("message", Matchers.equalTo("Resource not found 00000000-0000-0000-0000-000000000000"));
-//    }
+
+    @Test
+    public void should_receive_response_for_a_get_document_binary() {
+
+        RestAssured.baseURI = baseURI + "/cases/documents/" + documentId + "/binary";
+        RestAssured.useRelaxedHTTPSValidation();
+
+        Response response = SerenityRest
+            .given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .header("ServiceAuthorization", "Bearer " + getServiceAuth())
+            .header("Authorization", "Bearer " + userToken)
+            .when()
+            .get("/")
+            .andReturn();
+        response.then().assertThat().statusCode(HttpStatus.NOT_FOUND.value())
+            .body("message", Matchers.equalTo("Resource not found " + documentId));
+    }
+
+    @Test
+    public void should_receive_response_for_patch_ttl() {
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("ttl", "2025-10-31T10:10:10+0000");
+
+        RestAssured.baseURI = baseURI + "/cases/documents/" + documentId;
+        RestAssured.useRelaxedHTTPSValidation();
+
+        Response response = SerenityRest
+            .given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .header("ServiceAuthorization", "Bearer " + getServiceAuth())
+            .header("Authorization", "Bearer " + userToken)
+            .body(requestBody.toString())
+            .when()
+            .patch("/")
+            .andReturn();
+        response.then().assertThat().statusCode(HttpStatus.NOT_FOUND.value())
+            .body("message", Matchers.equalTo("Resource not found " + documentId));
+    }
+
+    @Test
+    public void should_receive_response_for_patch_attach_to_document() {
+
+        JSONObject document1 = new JSONObject();
+        document1.put("id", documentId);
+        document1.put("hashToken", "3aba5fe28560118793e7f086b442ec40f933c8607ac4e32dc4adf865a0be41c2");
+
+        JSONArray documents = new JSONArray();
+        documents.put(document1);
+
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("caseId", "1234123412341234");
+        requestBody.put("documents", documents);
+
+        RestAssured.baseURI = baseURI + "/cases/documents/" + documentId;
+        RestAssured.useRelaxedHTTPSValidation();
+
+        Response response = SerenityRest
+            .given()
+            .relaxedHTTPSValidation()
+            .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+            .header("ServiceAuthorization", "Bearer " + getServiceAuth())
+            .header("Authorization", "Bearer " + userToken)
+            .body(requestBody.toString())
+            .when()
+            .patch("/")
+            .andReturn();
+        response.then().assertThat().statusCode(HttpStatus.NOT_FOUND.value())
+            .body("message", Matchers.equalTo("Resource not found " + documentId));
+    }
+
+    private String getServiceAuth() {
+        return new BaseTest().authTokenGenerator(secret, microService, generateServiceAuthorisationApi(s2sUrl)).generate();
+    }
 }
