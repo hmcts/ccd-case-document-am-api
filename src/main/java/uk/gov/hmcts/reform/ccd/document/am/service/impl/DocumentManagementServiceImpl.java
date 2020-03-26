85d7c189676909fbda7814f84d93578815eca3e0
Nitin <nitin.sahu@hmcts.net>
2020-03-26 10:14:55 +0000

    AM-609_Fix_Sonar_Issues - upload parameters fixed

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/impl/DocumentManagementServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/service/impl/DocumentManagementServiceImpl.java
@@ -248,16 +248,14 @@ public class DocumentManagementServiceImpl implements DocumentManagementService 
                                                   String userId) {
 
         LinkedMultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
-        HttpHeaders headers = prepareRequestForUpload(
-            files,
-            classification,
-            roles,
-            serviceAuthorization,
-            caseTypeId,
-            jurisdictionId,
-            userId,
-            bodyMap
-                                                     );
+        HttpHeaders headers = prepareRequestForUpload(classification, roles, serviceAuthorization, caseTypeId, jurisdictionId,
+                                                      userId, bodyMap);
+
+        for (MultipartFile file : files) {
+            if (!file.isEmpty()) {
+                bodyMap.add(FILES, file.getResource());
+            }
+        }
         HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(bodyMap, headers);
         ResponseEntity<Object> uploadedDocumentResponse = restTemplate
             .postForEntity(documentURL.concat("/documents"), requestEntity, Object.class);
@@ -362,15 +360,11 @@ public class DocumentManagementServiceImpl implements DocumentManagementService 
         return request.getRequestURL().append("/").append(documentUrl).toString();
     }
 
-    private HttpHeaders prepareRequestForUpload(List<MultipartFile> files, String classification, List<String> roles,
+    private HttpHeaders prepareRequestForUpload(String classification, List<String> roles,
                                                 String serviceAuthorization,
                                                 String caseTypeId, String jurisdictionId, String userId,
                                                 LinkedMultiValueMap<String, Object> bodyMap) {
-        for (MultipartFile file : files) {
-            if (!file.isEmpty()) {
-                bodyMap.add(FILES, file.getResource());
-            }
-        }
+
 
         bodyMap.set(CLASSIFICATION, classification);
         bodyMap.set(ROLES, String.join(",", roles));
