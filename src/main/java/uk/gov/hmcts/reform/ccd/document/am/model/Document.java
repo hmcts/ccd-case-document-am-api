eec0278bbf3035777eb08eb7afcd9f64d5e92d2f
Nitish Singla <nitish.singla@hmcts.net>
2020-04-06 13:30:43 +0100

    Am-588: Rollback the changes in Document.java.

--- src/main/java/uk/gov/hmcts/reform/ccd/document/am/model/Document.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/document/am/model/Document.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.ccd.document.am.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
