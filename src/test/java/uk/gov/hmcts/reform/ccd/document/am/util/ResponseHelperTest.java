40e7addabab439ad3994a0f84e3c9097cc9ba5b3
Nitin <nitin.sahu@hmcts.net>
2020-04-08 10:41:54 +0100

    More coverage added

--- src/test/java/uk/gov/hmcts/reform/ccd/document/am/util/ResponseHelperTest.java
+++ src/test/java/uk/gov/hmcts/reform/ccd/document/am/util/ResponseHelperTest.java
@@ -2,7 +2,9 @@ package uk.gov.hmcts.reform.ccd.document.am.util;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import uk.gov.hmcts.reform.ccd.document.am.model.StoredDocumentHalResource;
 
@@ -14,6 +16,16 @@ import static org.junit.jupiter.api.Assertions.*;
 class ResponseHelperTest {
 
     @Test
+    void shouldGetToResponseEntity() {
+        StoredDocumentHalResource resource = new StoredDocumentHalResource();
+        ResponseEntity responseEntity = new ResponseEntity<StoredDocumentHalResource>(resource, getHttpHeaders(), HttpStatus.OK);
+        ResponseEntity result = ResponseHelper.toResponseEntity(responseEntity, UUID.fromString("f565abb5-c337-4ccb-ba78-1c43989e3bd6"));
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+        assertEquals(2, result.getHeaders().size());
+        assertNotNull(result.getBody());
+    }
+
+    @Test
     void convertHeaders() {
         MultiValueMap<String, String> result = ResponseHelper.convertHeaders(getHttpHeaders());
         assertEquals(2, result.size());
@@ -22,10 +34,17 @@ class ResponseHelperTest {
     }
 
     @Test
-    void addHateoasLinks() {
-        StoredDocumentHalResource name = new StoredDocumentHalResource();
-        Optional<StoredDocumentHalResource> opt = Optional.of(name);
+    void addHateoasLinksForOtherObjects() {
+        Optional<Object> opt = Optional.of(new Object());
         ResponseHelper.addHateoasLinks(opt, UUID.fromString("f565abb5-c337-4ccb-ba78-1c43989e3bd5"));
+        assertNotNull(opt);
+    }
+
+    @Test
+    void addHateoasLinksForEmpltyObjects() {
+        Optional<Object> opt = Optional.empty();
+        ResponseHelper.addHateoasLinks(opt, UUID.fromString("f565abb5-c337-4ccb-ba78-1c43989e3bd5"));
+        assertNotNull(opt);
     }
 
     private HttpHeaders getHttpHeaders() {
