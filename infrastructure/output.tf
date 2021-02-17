90d53baa9597055112c696dfec01b94dc392425e
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-02-17 15:27:29 +0000

    reverted infrastructure changes

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,27 @@
-output "env" {
-  value = "${var.env}"
+output "idam_url" {
+  value = "${var.idam_api_url}"
+}
+
+output "s2s_url" {
+  value = "${local.s2s_url}"
+}
+
+output "BEFTA_S2S_CLIENT_ID" {
+  value = "ccd_gw"
+}
+
+output "OAUTH2_CLIENT_ID" {
+  value = "ccd_gateway"
+}
+
+output "OAUTH2_REDIRECT_URI" {
+  value = "${local.oauth2_redirect_uri}"
+}
+
+output "DEFINITION_STORE_HOST" {
+  value = "${local.definition_store_host}"
+}
+
+output "DOCUMENT_STORE_URL" {
+  value = "${local.document_store_url}"
 }
