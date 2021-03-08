28c8f678e673c36b5d69a807bf80ecf9ea64c761
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-03-08 17:15:36 +0000

    RDM-11001 - syntax fix

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -4,14 +4,23 @@ variable "product" {
 variable "component" {
 }
 
+variable "env" {
+  description = "(Required) The environment in which to deploy the application infrastructure."
+}
+
 variable "location" {
   default = "UK South"
+}
+
+variable "tenant_id" {
+  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environment variables and not normally required to be specified."
+}
+
+variable "jenkins_AAD_objectId" {
+  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
 variable "raw_product" {
   default = "ccd" // jenkins-library overrides product for PRs and adds e.g. pr-118-ccd
 }
 
-variable "env" {
-type = "string"
-}
