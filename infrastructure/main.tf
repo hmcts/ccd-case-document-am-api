80e8fa6378c75874d090e210526191e3f7938b3d
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-03-09 10:31:57 +0000

    RDM-11001 - key-vault terraform fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,8 +8,8 @@ locals {
 }
 
 data "azurerm_key_vault" "ccd_shared_key_vault" {
-  name                = local.vaultName
-  resource_group_name = local.sharedResourceGroup
+  name                = "${local.vaultName}"
+  resource_group_name = "${local.sharedResourceGroup}"
 }
 
 data "azurerm_key_vault" "s2s_vault" {
@@ -19,12 +19,12 @@ data "azurerm_key_vault" "s2s_vault" {
 
 data "azurerm_key_vault_secret" "ccd-case-document-am-api-s2s-vault-secret" {
   name         = "microservicekey-ccd-case-document-am-api"
-  key_vault_id = data.azurerm_key_vault.s2s_vault.id
+  key_vault_id = "${data.azurerm_key_vault.s2s_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "ccd-case-document-am-api-s2s-secret" {
   name         = "ccd-case-document-am-api-s2s-secret"
-  value        = data.azurerm_key_vault_secret.ccd-case-document-am-api-s2s-vault-secret.value
-  key_vault_id = data.azurerm_key_vault.ccd_shared_key_vault.id
+  value        = "${data.azurerm_key_vault_secret.ccd-case-document-am-api-s2s-vault-secret.value}"
+  key_vault_id = "${data.azurerm_key_vault.ccd_shared_key_vault.id}"
 }
 
