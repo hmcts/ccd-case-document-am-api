88acfc6b25dec14a0238095a9d4eb19c8ec04cff
KleoG <kleogeorgiou@hotmail.com>
2021-06-14 17:07:38 +0100

    RDM-12413 - fix checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/documentam/BaseTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/documentam/BaseTest.java
@@ -9,21 +9,13 @@ import com.nimbusds.jwt.SignedJWT;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.TextCodec;
-import org.apache.commons.lang3.StringUtils;
-import org.junit.jupiter.api.BeforeEach;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContextImpl;
-import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -34,13 +26,11 @@ import uk.gov.hmcts.reform.ccd.documentam.configuration.AuditConfiguration;
 import uk.gov.hmcts.reform.ccd.documentam.utils.KeyGenUtil;
 
 import javax.inject.Inject;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames.ACCESS_TOKEN;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
@@ -68,25 +58,6 @@ public class BaseTest {
     @SpyBean
     @Inject
     protected AuditRepository auditRepository;
-
-//    @Value("${wiremock.server.port}")
-//    protected Integer wiremockPort;
-//    @Mock
-//    protected Authentication authentication;
-
-//    @BeforeEach
-//    void init() {
-//        Jwt jwt = dummyJwt();
-//        when(authentication.getPrincipal()).thenReturn(jwt);
-//        SecurityContextHolder.setContext(new SecurityContextImpl(authentication));
-//    }
-//
-//    private Jwt dummyJwt() {
-//        return Jwt.withTokenValue("a dummy jwt token")
-//                .claim("aClaim", "aClaim")
-//                .header("aHeader", "aHeader")
-//                .build();
-//    }
 
     public static HttpHeaders createHttpHeaders(String serviceName) throws JOSEException {
         return createHttpHeaders(AUTH_TOKEN_TTL, serviceName, AUTH_TOKEN_TTL);
