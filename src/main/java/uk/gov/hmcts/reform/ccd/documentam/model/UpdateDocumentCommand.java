28bc76d8b4d427e2c6752920f1af48fe32c28e55
Semenu Aggor <semenu.aggor@HMCTS.NET>
2021-07-13 23:16:03 +0100

    RDM-12663 Fix updated validation messages in functional test

--- src/main/java/uk/gov/hmcts/reform/ccd/documentam/model/UpdateDocumentCommand.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/documentam/model/UpdateDocumentCommand.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.ccd.documentam.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -18,7 +17,6 @@ import static uk.gov.hmcts.reform.ccd.documentam.apihelper.Constants.TIMESTAMP_F
 @AllArgsConstructor
 public class UpdateDocumentCommand {
     @ApiModelProperty
-    @JsonProperty("ttl")
     @NotNull(message = "Provide the TTL")
     @JsonFormat(pattern = TIMESTAMP_FORMAT)
     @DateTimeFormat(pattern = TIMESTAMP_FORMAT)
