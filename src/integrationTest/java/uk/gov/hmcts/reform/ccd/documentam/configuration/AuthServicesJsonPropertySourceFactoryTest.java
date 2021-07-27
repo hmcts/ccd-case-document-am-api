50f5be5f58b48a95227a4a14d4486b1d465fe3d0
Sateesh Mathangi <sateeshkumar.m@gmail.com>
2021-07-27 08:42:50 +0100

    minor tweaks to test and allowed all jurisdictions for xui_web

--- src/integrationTest/java/uk/gov/hmcts/reform/ccd/documentam/configuration/AuthServicesJsonPropertySourceFactoryTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/documentam/configuration/AuthServicesJsonPropertySourceFactoryTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.ccd.documentam.configuration;
 
 import org.junit.jupiter.api.Test;
 import uk.gov.hmcts.reform.ccd.documentam.BaseTest;
-import uk.gov.hmcts.reform.ccd.documentam.model.AuthorisedService;
 import uk.gov.hmcts.reform.ccd.documentam.model.AuthorisedServices;
 import uk.gov.hmcts.reform.ccd.documentam.model.enums.Permission;
 
@@ -14,7 +13,7 @@ class AuthServicesJsonPropertySourceFactoryTest extends BaseTest {
 
     private static final String SERVICE_ID = "xui_webapp";
     private static final String CASE_TYPE_ID_ALL_WILDCARD = "*";
-    private static final String JURISDICTION_ID = "BEFTA_JURISDICTION_2";
+    private static final String JURISDICTION_ID = "*";
 
     @Inject
     private AuthorisedServices authorisedServices;
@@ -22,14 +21,15 @@ class AuthServicesJsonPropertySourceFactoryTest extends BaseTest {
     @Test
     public void successfullyLoadServiceConfigFile() {
 
-        assertThat(authorisedServices).isNotNull()
-            .satisfies(x -> {
-                AuthorisedService authorisedService = x.getAuthServices().get(0);
-                assertThat(authorisedService.getId()).isEqualTo(SERVICE_ID);
-                assertThat(authorisedService.getCaseTypeId().size()).isEqualTo(1);
-                assertThat(authorisedService.getCaseTypeId().get(0)).isEqualTo(CASE_TYPE_ID_ALL_WILDCARD);
-                assertThat(authorisedService.getJurisdictionId()).isEqualTo(JURISDICTION_ID);
-                assertThat(authorisedService.getPermissions().get(0)).isEqualTo(Permission.CREATE);
+        assertThat(authorisedServices).isNotNull();
+        assertThat(authorisedServices.getAuthServices())
+            .first()
+            .satisfies(service -> {
+                assertThat(service.getId()).isEqualTo(SERVICE_ID);
+                assertThat(service.getCaseTypeId().size()).isEqualTo(1);
+                assertThat(service.getJurisdictionId()).isEqualTo(JURISDICTION_ID);
+                assertThat(service.getCaseTypeId()).contains(CASE_TYPE_ID_ALL_WILDCARD);
+                assertThat(service.getPermissions()).contains(Permission.CREATE);
             });
     }
 
