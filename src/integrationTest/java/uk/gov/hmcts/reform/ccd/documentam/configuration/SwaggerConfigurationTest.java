a3bfe83439ac12ea8cf517887ac7a1aec5831565
Mike Brown <michael.brown@hmcts.net>
2022-07-28 14:11:56 +0100

    CCD-3509 CVE-2021-22044 SwaggerConfigurationTest added for coverage, SwaggerConfiguration sonar.exclusions removed

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/ccd/documentam/configuration/SwaggerConfigurationTest.java
@@ -0,0 +1,61 @@
+package uk.gov.hmcts.reform.ccd.documentam.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.endpoint.web.EndpointMediaTypes;
+import org.springframework.boot.actuate.endpoint.web.WebEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.core.env.Environment;
+import uk.gov.hmcts.reform.ccd.documentam.BaseTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SwaggerConfigurationTest extends BaseTest {
+
+    @Autowired
+    private WebEndpointsSupplier webEndpointsSupplier;
+
+    @Autowired
+    private ServletEndpointsSupplier servletEndpointsSupplier;
+
+    @Autowired
+    private ControllerEndpointsSupplier controllerEndpointsSupplier;
+
+    @Autowired
+    private EndpointMediaTypes endpointMediaTypes;
+
+    @Autowired
+    private CorsEndpointProperties corsProperties;
+
+    @Autowired
+    private WebEndpointProperties webEndpointProperties;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private SwaggerConfiguration swaggerConfiguration;
+
+    @Test
+    public void successfullyLoadWebEndpointServletHandlerMappingTest() {
+        assertThat(webEndpointsSupplier).isNotNull();
+        assertThat(servletEndpointsSupplier).isNotNull();
+        assertThat(controllerEndpointsSupplier).isNotNull();
+        assertThat(endpointMediaTypes).isNotNull();
+        assertThat(corsProperties).isNotNull();
+        assertThat(webEndpointProperties).isNotNull();
+        assertThat(environment).isNotNull();
+        assertThat(swaggerConfiguration).isNotNull();
+
+        assertThat(swaggerConfiguration.webEndpointServletHandlerMapping(webEndpointsSupplier, servletEndpointsSupplier,
+            controllerEndpointsSupplier, endpointMediaTypes,corsProperties, webEndpointProperties, environment))
+            .satisfies(mapping -> {
+                assertThat(mapping.getEndpoints().size()).isEqualTo(2);
+                assertThat(mapping.getApplicationContext().getId()).isEqualTo("CCD Case Document AM API");
+            });
+    }
+
+}
