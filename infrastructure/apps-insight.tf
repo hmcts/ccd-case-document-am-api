1a0292c4f7a85b566d1c4754a622bb07315e4255
Dinesh Patel <dinesh1.patel@btinternet.com>
2023-10-10 13:48:31 +0100

    updated infra terraform

--- infrastructure/apps-insight.tf
+++ infrastructure/apps-insight.tf
@@ -7,3 +7,9 @@ data "azurerm_key_vault" "key_vault" {
   name                = "${var.product}-${var.env}"
   resource_group_name = "${var.product}-shared-${var.env}"
 }
+
+resource "azurerm_key_vault_secret" "app_insights_connection_string" {
+  name         = "app-insights-connection-string"
+  value        = azurerm_application_insights.appinsights.connection_string
+  key_vault_id = module.key-vault.key_vault_id
+}
