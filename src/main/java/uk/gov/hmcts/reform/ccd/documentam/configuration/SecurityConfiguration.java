8ebb0ee57401c8b93bcfbfd720d79386c70bfa0f
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2026-09-17 13:47:30 +0100

    CCD-8198 : Update BEFTA to 9.2.11 and idam-java-client to 3.1.0 for OIDC support

--- src/main/java/uk/gov/hmcts/reform/ccd/documentam/configuration/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/ccd/documentam/configuration/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.reform.ccd.documentam.configuration;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -10,9 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtClaimValidator;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.JwtIssuerValidator;
 import org.springframework.security.oauth2.jwt.JwtTimestampValidator;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
@@ -32,8 +36,8 @@ public class SecurityConfiguration {
     @Value("${spring.security.oauth2.client.provider.oidc.issuer-uri}")
     private String issuerUri;
 
-    @Value("${oidc.issuer}")
-    private String issuerOverride;
+    @Value("#{'${idam.security.allowed-issuers}'.split(',')}")
+    private List<String> allowedIssuers = new ArrayList<>();
 
     private final ServiceAuthFilter serviceAuthFilter;
     private final ExceptionHandlingFilter exceptionHandlingFilter;
@@ -91,10 +95,14 @@ public class SecurityConfiguration {
         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuerUri);
         // We are using issuerOverride instead of issuerUri as SIDAM has the wrong issuer at the moment
         OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
-        OAuth2TokenValidator<Jwt> withIssuer = new JwtIssuerValidator(issuerOverride);
-        // OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withIssuer);
-        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp);
+        OAuth2TokenValidator<Jwt> validator = 
+            new DelegatingOAuth2TokenValidator<>(withTimestamp, allowedIssuersValidator());
         jwtDecoder.setJwtValidator(validator);
         return jwtDecoder;
+    }
+
+    OAuth2TokenValidator<Jwt> allowedIssuersValidator() {
+        Set<String> allowedIssuersSet = new HashSet<>(allowedIssuers);
+        return new JwtClaimValidator<>("iss", allowedIssuersSet::contains);
     }
 }
